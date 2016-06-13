b3b4ee6b3ea97df6a3d24638153e0c9ac3f4cc6e
Kolopox <kolopox@gmail.com>
2016-06-13 17:32:40 +0200

    First try View

--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -131,7 +131,7 @@ class ViewFrame extends JFrame implements KeyListener {
 		this.setResizable(false);
 		this.addKeyListener(this);
 		this.setContentPane(new ViewPanel(this));
-		this.setSize(400 + this.getInsets().left + this.getInsets().right, 60 + this.getInsets().top + this.getInsets().bottom);
+		this.setSize(480 + 10,384+10);//400 + this.getInsets().left + this.getInsets().right, 60 + this.getInsets().top + this.getInsets().bottom); //384 par 640
 		this.setLocationRelativeTo(null);
 	}
 
